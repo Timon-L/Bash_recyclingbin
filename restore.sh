e3fd8ec2bf72cefc53c2409e5979bc0147e0f355
tim <liuchingchun96@gmail.com>
2023-07-20 10:25:21 +0800

    Restore now works

--- restore.sh
+++ restore.sh
@@ -10,20 +10,19 @@ function restore_file(){
 	local file_name=$(basename $1)
 	local name_in_bin="$BIN_PATH/$2"
 	local name_to_res="$BIN_PATH/$file_name"
-	echo "Arg1: $1"
-	echo "name in bin: $name_in_bin"
-	echo "file name in res: $file_name"
-	echo "file name to restore: $name_to_res"
+	#echo "Arg1: $1"
+	#echo "name in bin: $name_in_bin"
+	#echo "file name in res: $file_name"
+	#echo "file name to restore: $name_to_res"
 
 	if mv $name_in_bin $name_to_res ; then #rename file to original
-		#if mv "$BIN_PATH/$file_name" $dir_path ; then
-			#grep -wv "$2" $RES_PATH > $TEMP_FILE
-        	       	#cat $TEMP_FILE > $RES_PATH
-		#else
-			#echo "Error, file not restored"
-			#exit 1
-		#fi
-		echo "CONT"
+		if mv "$BIN_PATH/$file_name" $dir_path ; then
+			grep -wv "$2" $RES_PATH > $TEMP_FILE
+        	       	cat $TEMP_FILE > $RES_PATH
+		else
+			echo "Error, file not restored"
+			exit 1
+		fi
 	else
 		echo "Error, cant rename file"
 		exit 1
@@ -52,7 +51,7 @@ function search_dir(){
 function check_restore(){
 	if grep -wq "$1:" $RES_PATH ; then #Look for filename_inode
 		file_path=$(grep -w "$1:" $RES_PATH | cut -d: -f2) #Absolute file path
-		echo $file_path
+		#echo $file_path
 		name_w_node="$1"
 		#echo $name_w_node
 		if search_dir $file_path; then
